c0198ae2ca63e1f189c27799338db22420a3b6c3
mrogowski11 <rogowskimarcin11@gmail.com>
2022-02-07 16:02:35 +0100

    Fish init

---
+++ fish/config.fish
@@ -0,0 +1,35 @@
+set -g -x fish_greeting ''
+
+alias vi="nvim"
+alias vim="nvim"
+
+set -gx TERM xterm-256color
+
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+alias g git
+command -qv nvim && alias vim nvim
+
+set -gx EDITOR nvim
+
+set -gx PATH bin $PATH
+set -gx PATH ~/bin $PATH
+set -gx PATH ~/.local/bin $PATH
+
+switch (uname)
+  case Darwin
+    source (dirname (status --current-filename))/config-osx.fish
+  case Linux
+    source (dirname (status --current-filename))/config-linux.fish
+  case '*'
+    source (dirname (status --current-filename))/config-windows.fish
+end
+
+set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
+if test -f $LOCAL_CONFIG
+  source $LOCAL_CONFIG
+end
+
+starship init fish | source

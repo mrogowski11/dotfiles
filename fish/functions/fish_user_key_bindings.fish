d82d448a362eaf7a9234a6638ae0e50be7f28a35
mrogowski11 <rogowskimarcin11@gmail.com>
2022-04-22 21:30:55 +0200

    Fish keybind changes

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,8 +1,9 @@
 function fish_user_key_bindings
   # peco
   bind \cr peco_select_history # Bind for peco select history to Ctrl+R
-  bind \cf peco_change_directory # Bind for peco change directory to Ctrl+F
-  bind \ce peco_select_file # Bind for peco select file to Ctrl+E
+  bind \ct peco_select_file # Bind for peco select file to Ctrl+T
+  bind \cy peco_change_directory # Bind for peco change directory to Ctrl+Y
+
 
   # vim-like
   bind \cl forward-char

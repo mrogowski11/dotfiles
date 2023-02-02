d176e76438060c1192dcbbe7e4a381234e5f20c9
Marcin, Rogowski <rogowskimarcin11@gmail.com>
2023-02-02 13:56:29 +0100

    feat(nvim): change leader to space + minor changes

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -1,21 +1,31 @@
 vim.cmd("autocmd!")
+vim.g.mapleader = " "
 
 vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
+vim.opt.encoding = 'utf-8' 
 vim.opt.fileencoding = 'utf-8'
 
-vim.wo.number = true
+vim.opt.nu = true
+vim.opt.relativenumber = true
 
 vim.opt.title = true
-vim.opt.autoindent = true
+vim.opt.autoindent = true 
 vim.opt.smartindent = true
-vim.opt.hlsearch = true
+
+vim.opt.incsearch = true
+vim.opt.hlsearch = false 
+
+vim.opt.termguicolors = true
+
 vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.nvim/undodir"
+vim.opt.undofile = true
+
 vim.opt.showcmd = true
 vim.opt.cmdheight = 1
 vim.opt.laststatus = 2
-vim.opt.expandtab = true
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 10 
 vim.opt.shell = 'fish'
 vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
 vim.opt.inccommand = 'split'
@@ -25,10 +35,15 @@ vim.opt.breakindent = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.wrap = false -- No Wrap lines
+vim.opt.expandtab = true
+vim.opt.wrap = false 
 vim.opt.backspace = { 'start', 'eol', 'indent' }
 vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
 vim.opt.wildignore:append { '*/node_modules/*' }
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])

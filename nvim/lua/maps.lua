2f35e27718994bff217afae3619c75ec05981f09
Marcin, Rogowski <rogowskimarcin11@gmail.com>
2023-02-02 13:57:30 +0100

    feat(nvim): keybinds for yanking to buffer, moving around file

--- nvim/lua/maps.lua
+++ nvim/lua/maps.lua
@@ -1,7 +1,5 @@
 local keymap = vim.keymap
 
-vim.g.mapleader = ','
-
 keymap.set('n', 'x', '"_x')
 
 -- Increment/decrement
@@ -13,6 +11,14 @@ keymap.set('n', 'dw', 'vb"_d')
 
 -- Select all
 keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Copy over word without losing buffer
+keymap.set('x', '<leader>p', '\"_dP')
+
+-- Yank to global clipboard
+keymap.set('n', '<leader>y', '\"+y')
+keymap.set('v', '<leader>y', '\"+y')
+keymap.set('n', '<leader>Y', '\"+Y')
 
 -- Save with root permission (not working for now)
 --vim.api.nvim_create_user_command('W', 'w !sudo tee > /dev/null %', {})
@@ -34,6 +40,21 @@ keymap.set('n', '<C-w><left>', '<C-w><')
 keymap.set('n', '<C-w><right>', '<C-w>>')
 keymap.set('n', '<C-w><up>', '<C-w>+')
 keymap.set('n', '<C-w><down>', '<C-w>-')
+
+-- Move highlighted lines
+keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+
+-- Move below line after current line, don't change cursor's place
+keymap.set('n', 'J', "mzJ'z")
+
+-- Keep cursor at the middle when jumping
+keymap.set('n', '<C-d>', '<C-d>zz')
+keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Keep cursor at the middle when searching
+keymap.set('n', 'n', 'nzzzv')
+keymap.set('n', 'N', 'Nzzzv')
 
 -- trouble plugin
 keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>',
